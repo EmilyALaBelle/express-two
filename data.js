f600194e3b22b8cf9b7d29c6321f9e819d29f85f
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-17 16:14:06 -0400

    new API

---
+++ data.js
@@ -0,0 +1,25 @@
+export default[{
+    "name": "Brad Jones",
+    "age": 30,
+    "birthday": "October 15, 1992"
+}, {
+    "name": "Fanessa Hilaire",
+    "age": 28,
+    "Birthday": "May 25, 1994"
+
+},{
+    "name": "Captain Ed",
+    "age": 25,
+    "Birthday": "July 8, 1997"
+
+},{
+    "name": "Todd Albert",
+    "age": 47,
+    "birthday": "October, 17, 1975"
+}, {
+    "name": "Gianna Davila",
+    "age": 18,
+    "birthday": "September 1, 2004"
+}]
+
+
